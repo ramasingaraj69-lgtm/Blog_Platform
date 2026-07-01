92ba3843aefd861cc25ea420ecb2971ad8cade5f
ramasingaraj69@gmail.com <ramasingaraj69@gmail.com>
2026-07-01 20:23:18 +0530

    Deployment Setup

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "https://blog-platform-zrph.onrender.com/api",
 });
 
 export default API;
