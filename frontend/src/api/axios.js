5ce464708da0dddbfeeed3b2a6f056508d59c5f7
Rama <ramasingaraj69@gmail.com>
2026-07-27 16:31:31 +0530

    API Endpoints

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
+    //baseRL: "https://8000/api/"
+     baseURL: "https://blog-platform-zrph.onrender.com/api/",
 
-    baseURL: "https://blog-platform-zrph.onrender.com/api/",
 });
 
 export default API;
