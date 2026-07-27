f2041dd9cff6ade11cc70406659774214cc3e870
Rama <ramasingaraj69@gmail.com>
2026-07-27 17:47:25 +0530

    final deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     //baseRL: "https://8000/api/"
-    baseURL: "https://blog-platform-zrph.onrender.com/",
+    baseURL: "https://blog-platform-zrph.onrender.com/api/",
 
 });
 
